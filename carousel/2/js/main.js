95d13be57d0072c53cb08ba291ba958822df22aa
Kalpesh Khandagale <kalpesh@brandmovers.in>
2022-07-05 12:28:35 +0530

    Updated slider scripts

--- carousel/2/js/main.js
+++ carousel/2/js/main.js
@@ -2,6 +2,9 @@ const carousel = document.querySelector('.carousel')
 const previousButton = carousel.querySelector('.previous-button')
 const nextButton = carousel.querySelector('.next-button')
 const contents = document.querySelector('.carousel__contents')
+const dots = Array.from(carousel.querySelectorAll('.carousel__dot'))
+const slides = Array.from(carousel.querySelectorAll('.carousel__slide'))
+const dotsContainer = carousel.querySelector('.carousel__dots')
 
 nextButton.addEventListener('click', event => {
     const currentSlide = contents.querySelector('.is-selected')
@@ -20,6 +23,12 @@ nextButton.addEventListener('click', event => {
     if(!nextSlide.nextElementSibling) {
         nextButton.setAttribute('hidden', true)
     }
+
+    // Highlight dot
+    const currentDot = dotsContainer.querySelector('.is-selected')
+    const nextDot = currentDot.nextElementSibling
+    currentDot.classList.remove('is-selected')
+    nextDot.classList.add('is-selected')
 })
 
 previousButton.addEventListener('click', event => {
@@ -39,5 +48,50 @@ previousButton.addEventListener('click', event => {
     if (!previousSlide.previousElementSibling) {
         previousButton.setAttribute('hidden', true)
     }
+
+    // Highlight dot
+    const currentDot = dotsContainer.querySelector('.is-selected')
+    const previousDot = currentDot.previousElementSibling
+    currentDot.classList.remove('is-selected')
+    previousDot.classList.add('is-selected')
 })
 
+
+dots.forEach(dot => {
+    dot.addEventListener('click', event => {
+        let clickedDotIndex
+
+        for (let index = 0; index < dots.length; index++) {
+            if (dots[index] === dot) {
+                clickedDotIndex = index
+            }
+        }
+
+        const slideShow = slides[clickedDotIndex]
+        const destination = getComputedStyle(slideShow).left
+        const currentSlide = contents.querySelector('.is-selected')
+        currentSlide.classList.remove('is-selected')
+
+        slideShow.classList.add('is-selected')
+        contents.style.left = '-' + destination
+
+        dots.forEach(d => {
+            d.classList.remove('is-selected')
+        })
+
+        dot.classList.add('is-selected')
+
+        // Show / hide buttons
+
+        if (clickedDotIndex === 0) {
+            previousButton.setAttribute('hidden', true)
+            nextButton.removeAttribute('hidden')
+        } else if (clickedDotIndex === dots.length - 1) {
+            previousButton.removeAttribute('hidden')
+            nextButton.setAttribute('hidden', true)
+        } else {
+            previousButton.removeAttribute('hidden')
+            nextButton.removeAttribute('hidden')
+        }
+    })
+})
