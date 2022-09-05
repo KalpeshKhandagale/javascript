83b4865f0ae257a0d8159143fb619c3cd77659f2
Kalpesh Khandagale <kalpesh@brandmovers.in>
2022-09-05 17:03:18 +0530

    accordion & modal JS Updated

--- modal/modal-events/js/main.js
+++ modal/modal-events/js/main.js
@@ -2,9 +2,12 @@ const modalButton = document.querySelector('.jsModalButton')
 const modalCloseButton = document.querySelector('.jsModalClose')
 const modalOverlay = document.querySelector('.modal-overlay')
 const modal = document.querySelector('.modal')
+const  wavingHand = document.querySelector('.wave-hand')
+
 
 modalButton.addEventListener('click', event => {
   document.body.classList.add('modal-is-open')
+  wave()
 })
 
 modalCloseButton.addEventListener('click', event => {
@@ -18,6 +21,27 @@ modalOverlay.addEventListener('click', event => {
     document.body.classList.remove('modal-is-open')
   } 
 })
+
+
+// Wave hand animation here
+      function wave () {
+        const tl = new TimelineMax({})
+
+        tl.set(wavingHand, {transformOrigin: 'bottom center'})
+        tl.from(wavingHand, 0.5, {
+          scale : 0.25,
+          opacity : 0,
+          ease: Back.easeOut.config(1.5)
+        })
+
+        tl.to(wavingHand, 0.2, { rotation: 15 })
+        tl.to(wavingHand, 0.2, { rotation: -15 })
+        tl.to(wavingHand, 0.2, { rotation: 15 })
+        tl.to(wavingHand, 0.2, { rotation: -15 })
+        tl.to(wavingHand, 0.2, { rotation: 0 })
+      }
+
+
 
 
 // modal.addEventListener('click', event => {
