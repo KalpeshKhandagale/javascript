f7ba04212a4641cd870d303fdd4df7186e95d8c2
Kalpesh <kalpesh@brandmovers.in>
2026-03-16 21:25:08 +0530

    Forkify app loading a recipe from API

--- udemy/18-forkify/starter/src/js/controller.js
+++ udemy/18-forkify/starter/src/js/controller.js
@@ -11,3 +11,30 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function() {
+  try {
+   const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e882f');
+   const data = await res.json();
+
+   if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+   console.log(res, data);
+   let {recipe} = data.data;
+   recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+   }
+   console.log(recipe);
+  } catch(err) {
+    alert(err);
+  }
+}
+
+showRecipe();
