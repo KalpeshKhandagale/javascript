3f3e9b043355ede91c7f5f8a6ec6c11874363143
Kalpesh <kalpesh@brandmovers.in>
2026-04-11 14:27:23 +0530

    Forkify app implementing search result completed

--- udemy/18-forkify/starter/src/js/controller.js
+++ udemy/18-forkify/starter/src/js/controller.js
@@ -1,10 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
+
+if(module.hot) {
+  module.hot.accept();
+}
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -40,6 +45,9 @@ const controlRecipes = async function() {
 
 const controlSearchResults = async function() {
   try {
+    resultsView.renderSpinner();
+    console.log(resultsView);
+
     //1) Get search query
     const query = searchView.getQuery();
     if(!query) return;
@@ -48,12 +56,11 @@ const controlSearchResults = async function() {
     await model.loadSearchResults(query);
 
     // 3) Render results 
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch(err) {
     console.log(err);
   }
 };
-controlSearchResults();
 
 const init = function() {
     recipeView.addHandlerRender(controlRecipes);
