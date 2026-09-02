87313fc786bf0e081a4aedb6a439be1b945e6854
Kalpesh <kalpesh@brandmovers.in>
2026-09-02 16:14:21 +0530

    Forkify app uploading recipe completed

--- udemy/18-forkify/starter/src/js/config.js
+++ udemy/18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = "5f5f8c41-f7de-43be-bfcb-190bf2b44bdb";
+export const MODAL_CLOSE_SEC = 2.5;
